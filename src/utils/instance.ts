febe9f6051880ee83197595fe8ee4738a613d252
shocyber <143344306+shocyber@users.noreply.github.com>
2023-08-27 16:57:22 +0000

    Update instance.ts

--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://blinkit-api-mecf.onrender.com",
+  baseURL: "https://blinkit-api-2x5r.onrender.com",
 });
 export default instance;
